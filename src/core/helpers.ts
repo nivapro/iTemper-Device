3725395a367b12ccd42b8b7824acbff9c5e5971f
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-02-14 01:12:07 +0100

    Allow Unknown data type

--- src/core/helpers.ts
+++ src/core/helpers.ts
@@ -1,5 +1,5 @@
 import { log } from './logger';
-export function stringify(o: object) {
+export function stringify(o: unknown) {
     const cache: object[] = [];
 
     return JSON.stringify(o, function(key, value) {
