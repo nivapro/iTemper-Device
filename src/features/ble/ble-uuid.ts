67013cceb27f99c62410e54cef57501a1e7d2a0e
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-02-15 15:28:30 +0100

    New UUIDs

--- src/features/ble/ble-uuid.ts
+++ src/features/ble/ble-uuid.ts
@@ -1,18 +1,5 @@
-// 0a1f294a-abe8-4da5-98aa-149de8e76976
-// 55554474-e09f-423b-aedf-9dc249e79dd9
-// deeb26b8-b4b0-4884-a1e1-b13c0c957805
-// 00e5394a-2d99-4c2c-9c44-d752b618fc9b
-// cdd1fc10-6d5e-4254-8886-4d4130897b55
-// 8aa48c24-9d5d-4a92-847a-f866d54f4a9e
-// 6db7fe5f-7add-4d88-9d54-99dc2420275d
-// a449e701-371a-48b4-a8a5-e8105127c123
-// 09a52499-dbb2-49be-a308-d1a25d92dc67
-// 64bebe3d-7dcd-43c6-80a8-b2949a3b97ed
-
-// a449e701-371a-48b4-a8a5-e8105127c123
-// ffffffff-ffff-ffff-ffff-fffffffffff0
+// All UUIDs are based on this random UUID:
 // 1ad01b31-dd4b-478c-9aa3-cbca25db3739
-// 1ad01b31-dd4b-478c-9aa3-12bd90900000
 
 import { conf } from '../../core/config' 
 export enum UUID_Designator {
@@ -24,13 +11,13 @@ export enum UUID_Designator {
 export function getUuid(designator: UUID_Designator) {
     switch (designator) {
         case UUID_Designator.PrimaryService:
-            return conf.PRIMARY_SERVICE.length >  0 ?  conf.PRIMARY_SERVICE : '1ad01b31-dd4b-478c-9aa3-12bd90900100';
+            return conf.PRIMARY_SERVICE.length >  0 ?  conf.PRIMARY_SERVICE : '1ad01000-dd4b-478c-9aa3-cbca25db3739';
         case UUID_Designator.DeviceInfo:
-            return '1ad01b31-dd4b-478c-9aa3-12bd90900010';
+            return '1ad01010-dd4b-478c-9aa3-cbca25db3739';
         case UUID_Designator.CurrentWiFi:
-            return '1ad01b31-dd4b-478c-9aa3-12bd90900011';
+            return '1ad01020-dd4b-478c-9aa3-cbca25db3739';
         case UUID_Designator.AvailableWiFi:
-            return '1ad01b31-dd4b-478c-9aa3-12bd90900012';
+            return '1ad01030-dd4b-478c-9aa3-cbca25db3739';
     }
 }
 
