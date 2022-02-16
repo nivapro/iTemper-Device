39d3375fabea8b40e0cea84218d0f4aaa6f8b94e
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-02-16 08:27:15 +0100

    Move system bus to gatt-utils

--- src/features/ble/gatt/gatt-utils.ts
+++ src/features/ble/gatt/gatt-utils.ts
@@ -8,6 +8,23 @@ function label(f: string = ""){
     return m + "." + f + ": ";
 } 
 
+const systemBus: dbus.MessageBus = dbus.systemBus();
+export async function setBusName(name: string ){
+  const BUS_NAME = name;
+  systemBus.requestName(BUS_NAME, 0);
+  try{
+    await this._bus.requestName(BUS_NAME, 0);
+    log.info(label("setDBusName") + 'DBus name set to ' + BUS_NAME);
+  }
+  catch (e){
+    log.error(label("setDBusName") + "Could not request name, error=" + JSON.stringify(e));
+  }
+}
+export function getBus(): dbus.MessageBus{
+  return systemBus;
+} 
+
+
 export type DbusMembers = {
     properties?: { [key: string]: dbus.interface.PropertyOptions},
     methods?: { [key: string]: dbus.interface.MethodOptions },
