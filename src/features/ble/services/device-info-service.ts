af09b2a2d7b25711fbbd1ca741fe607cbdd40a0c
Torbjörn Vading <torbjorn.vading@nivapro.com>
2022-02-16 08:26:50 +0100

    Implement WiFi characteristics

--- src/features/ble/services/device-info-service.ts
+++ src/features/ble/services/device-info-service.ts
@@ -2,23 +2,23 @@
 import * as gatt from './../gatt'
 
 import { getUuid, UUID_Designator} from '../ble-uuid';
-// import { AvailableWiFiCharacteristic } from '../characteristics/available-wifi-characteristic';
-// import { CurrentWiFiCharacteristic } from '../characteristics/current-wifi-characteristic';
-import { DeviceCharacteristic } from '../characteristics/device-characteristic';
+import { AvailableWiFiCharacteristic } from '../characteristics/available-wifi';
+import { CurrentWiFiCharacteristic } from '../characteristics/current-wifi';
+import { DeviceInfoCharacteristic } from '../characteristics/device-info';
 
 const DOMAIN_PATH = '/io/itemper'; 
+const DOMAIN_NAME = 'io.itemper'; 
 //
 // All services, characteristics, and descriptors are located under this path.
 
-export const SERVICE0_UUID = '1ad01b31-dd4b-478c-9aa3-12bd90900100' // getUuid(UUID_Designator.PrimaryService);;
+export const SERVICE0_UUID = getUuid(UUID_Designator.PrimaryService);;
 export class GattServer {
-    _app = new gatt.Application(DOMAIN_PATH);
+    _app = new gatt.Application(DOMAIN_NAME , DOMAIN_PATH);
     _service = new gatt.Service(SERVICE0_UUID, this._app);
-    _char = new DeviceCharacteristic(this._service);
-
+    _deviceInfoChar = new DeviceInfoCharacteristic(this._service);
+    _currentWiFiChar = new CurrentWiFiCharacteristic(this._service);
+    _availableWiFiChar = new AvailableWiFiCharacteristic(this._service);
     public init(){
         this._app.init();
     } 
 } 
-
-
